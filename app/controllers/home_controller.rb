81a2d17801ca59d737272b15a98d2cae2931189f
hassanse91 <senhajihassan10@gmail.com>
2016-08-14 21:14:32 +0000

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,29 @@
 class HomeController < ApplicationController
+  
   def index
-  	@links = Link.where(user_id: current_user.id) if user_signed_in?
+  	@links = Link.where(user_id: current_user.id).order('id DESC')  if user_signed_in?
+    @users = User.all if user_signed_in?
+  	@posts = Post.where(user_id: current_user.id).order('id DESC')  if user_signed_in?
+
   end
+
+
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.user = current_user
+    @post.save
+
+      if @post.save
+        redirect_to root_post
+      else
+       render 'new'
+      end
+   
+    end
+
+
+ def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+  resources :posts
   resources :links
   root 'home#index'
+  resources :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
